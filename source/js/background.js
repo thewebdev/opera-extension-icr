3fbe1b91e95d103355e51ecf92cbf204f285ff8d
Shabeer <thewebdev@myopera.com>
2012-06-11 06:01:08 +0530

    working version 1

---
+++ source/js/background.js
@@ -0,0 +1,445 @@
+﻿var timeIt = null; // data refresh timer
+var delay;  // retry data access timer
+var slider; // slide time delay
+
+function $(v) {
+	if (document.getElementById(v)) {
+		return document.getElementById(v);
+	}
+}
+
+function E(v) {
+	return document.createElement(v);
+}
+
+function Txt(v) {
+	return document.createTextNode(v);
+}
+
+function hide(id) {
+	$(id).style.display = 'none';
+}
+
+function show(id) {
+	$(id).style.display = 'block';
+}
+
+function trueRound(value){
+	var digits = parseInt(widget.preferences.roundoff);
+	console.log("Digit: " + digits);
+    return (Math.round((value*Math.pow(10,digits)).toFixed(digits-1))/Math.pow(10,digits)).toFixed(digits);
+}
+
+function createDl(kids) {
+	var dl, dt, dd, txt, temp, temp1;
+	var inHtml = document.createDocumentFragment();
+	var list = $("rateSlides");
+	
+	if ($("rateSlides")) {
+		// if dl node exists
+		temp = $("rateSlides").getElementsByTagName('dt');
+		console.log("bTempType: " + typeof(temp));
+		
+		if (temp.length == kids) {
+			console.log("Nodes: equal");
+			return;
+		} else if (temp.length < kids) {
+			// add more dt dd nodes
+			
+			var z = kids - temp.length;
+			console.log("Nodes: " + z + " added");
+			
+			for (var a = 0; a < z; a++) {
+				dt = E('dt');
+				txt = Txt('');
+				dt.appendChild(txt);
+				
+				inHtml.appendChild(dt);
+				
+				dd = E('dd');
+				txt = Txt('');
+				dd.appendChild(txt);
+				
+				inHtml.appendChild(dd);				
+			}
+			
+			$("rateSlides").appendChild(inHtml);
+			return;
+			
+		} else if (temp.length > kids) {
+			// delete some dt dd nodes
+			
+			temp1 = $("rateSlides").getElementsByTagName('dd');
+			console.log("bTemp1Type: " + typeof(temp1));
+			
+			var x = temp.length - kids;
+			console.log("Nodes: " + x + " deleted");
+			
+			while (x != 0) {
+				$("rateSlides").removeChild(temp[0]);
+				$("rateSlides").removeChild(temp1[0]);
+				x -= 1;
+			}
+			
+			return;
+		}
+	} 
+	
+	// create and add to the DOM
+		
+	dl = E('dl');
+	dl.setAttribute('id', 'rateSlides');
+	
+	for (var i = 0; i < kids; i++) {
+		dt = E('dt');
+		txt = Txt('');
+		dt.appendChild(txt);
+		
+		dl.appendChild(dt);
+		
+		dd = E('dd');
+		txt = Txt('');
+		dd.appendChild(txt);
+		
+		dl.appendChild(dd);
+	}
+
+	inHtml.appendChild(dl);
+	$('data').appendChild(inHtml);
+	
+	return;
+}
+
+function update(input) {
+	var temp1;
+	var temp2;
+	var temp3;
+	
+	var first;
+	var second;
+	var pairs;
+	
+	var rateList;
+	var listCount;
+	var resources;
+	var fields;
+	
+	var parsedList = new Object;
+	var out = [];		
+	
+	if (input) {
+		//console.log("List: " + input);
+		// parse data
+	
+		// get count of currencies in data
+		listCount = input.list.meta.count; 
+		resources = input.list.resources;
+		
+		//console.log("Count: " + listCount);
+		
+		// 1. Extract necessary fields - symbol, price & change
+		for (var r = 0; r < listCount; r++) {
+			fields = resources[r].resource.fields;
+			
+			temp3 = fields.symbol.trim();
+			temp3 = temp3.substr(0, 3);
+			
+			temp1 = parseFloat(fields.price);
+			temp2 = parseFloat(fields.change);
+			
+			parsedList[temp3] = [temp1, temp2];
+		}
+		
+		//console.log("Parsed: " + parsedList);
+		
+		// 2. Get the user specified currency pairs
+		pairs = widget.preferences.pairs;
+		pairs = JSON.parse(pairs);
+		
+		console.log("bPairs: " + pairs);
+		console.log("bType: " + typeof(pairs));
+		
+		for (var i = 0; i < pairs.length; i++) {
+			
+			temp1 = pairs[i];
+			console.log("bPairs[i]: " + pairs[i]);
+			temp1 = temp1.split('/');
+			console.log("bSplit: " + pairs[i] + "=" + temp1);
+			
+			first = temp1[0];
+			second = temp1[1];
+			
+			console.log("bPair val: " + first + ' / ' + second);
+			
+			temp1 = first + " / " + second;
+			
+			//console.log("Currency: " + temp1 + '-' + i + first + second);
+			//console.log("Parse: " + parsedList[second][1])
+			
+			// USD is the base currency
+			if (first === "USD") { // Scenario 1: USD / x
+				
+				if (parsedList[second][1] < 0) { state = "stronger"; } 
+				if (parsedList[second][1] > 0) { state = "weaker"; }
+				if (parsedList[second][1] == 0) { state = "same"; }
+				
+				out[i] = [first, parsedList[second][0], second, state];
+
+			} else { // Scenario 2: x / USD
+				
+				if (parsedList[first][1] > 0) { state = "stronger"; } 
+				if (parsedList[first][1] < 0) { state = "weaker"; }
+				if (parsedList[first][1] == 0) { state = "same"; }
+				
+				out[i] = [first, 1/parsedList[first][0], second, state];
+			} 
+			
+			// Scenario 3: x / x (cross rate)
+			if ((first != "USD") && (second != "USD")) {
+				// price calculation with USD as base
+				
+				// Current value
+				temp2 = parsedList[second][0]/parsedList[first][0];
+				
+				// Previous value
+				temp3 = (parsedList[second][0] + (parsedList[second][1])) / (parsedList[first][0] + (parsedList[first][1]));
+
+				//console.log("Change: " + temp2 + ',' + temp3);
+				
+				// if the current value is more,
+				// it indicates that the first currency
+				// has weakened and vice versa.
+				if (temp2 > temp3) { state = "stronger"; } 
+				if (temp2 < temp3) { state = "weaker"; }
+				if (temp2 == temp3) { state = "same"; }
+				
+				out[i] = [first, temp2, second, state];
+			}
+		}
+		
+		//console.log("Output: " + out);
+		refDial("show", out);
+		
+	} else {
+		refDial("hang");
+	}
+}
+
+// get json feed from Yahoo Finance
+function getData() {
+	var data;
+	var url = "http://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote;currency=true?view=basic&format=json";
+	
+	refDial('wait');
+	var ext = new XMLHttpRequest();
+
+	ext.open('GET', url, true);
+	
+	ext.onreadystatechange = function (event) {
+		if (this.readyState == 4) {
+			if (this.status == 200 && this.responseText) {
+				data = JSON.parse(this.responseText);
+				update(data);
+			} else {
+				refDial('hang');
+			}
+		}
+	}
+
+	ext.send();	
+	
+	// console.log ("State: " + ext.readyState + ', Status: ' + ext.status); 
+	// console.log("Data: " + data);
+	
+	return data;
+}
+
+// display in speed dial
+function refDial(cmd, out) {
+	
+	// display exchange rate data
+	if (cmd === "show") {
+		//console.log("Cmd show");
+		var cls, dt, dd;
+		
+		clearInterval(slider);	
+		
+		createDl(out.length);
+		console.log("Out: " + out.length);
+		
+		dt = $("rateSlides").getElementsByTagName('dt');
+		dd = $("rateSlides").getElementsByTagName('dd');
+		
+		console.log("Dt/Dd: " + dt.length + ', ' + dd.length);
+		
+		for (var o = 0; o < out.length; o++) {
+
+			console.log("Dt[o]: " + dt[o]);
+			console.log("Out[o]: " + out[o]);
+			
+			if (dt[o]) {
+				// remove old css class
+				dt[o].className = "";
+				// assign new data
+				dt[o].innerHTML = '<span>1</span> ' + out[o][0];
+			} else {
+				console.log("Not Assigned to Dt!");
+			}
+			
+			if (dd[o]) {
+				// remove old css class
+				dd[o].className = "";	
+				// assign new data				
+				dd[o].innerHTML = '<span class="' + out[o][3] + '">' + trueRound(out[o][1]) + '</span> ' + out[o][2];
+			} else {
+				console.log("Not Assigned to Dd!");
+			}			
+		}
+		
+		dt[0].className = 'current';
+		dd[0].className = 'current';
+		
+		//output.innerHTML = html;
+		hide("wait");
+		show("data");
+		
+		slider = setInterval(startSlide, 5000);
+		startSlide(out.length);
+		return
+	}
+	
+	// indicate data refresh
+	if (cmd === "wait") {
+
+		//console.log("Cmd wait");
+		
+		//html = '<div><img src="pix/loading.gif" alt="Loading ...">';
+		//html = html + '<p>updating</p>';
+		//output.innerHTML = html;
+		
+		$("msg").firstChild.nodeValue = "updating";
+
+		clearInterval(slider);			
+		hide("data");
+		show("wait");
+		
+		return
+	}
+	
+	// indicate network error
+	if (cmd === "hang") {
+		//html = '<div><img src="pix/loading.gif" alt="Loading ...">';
+		//html = html + '<p>hung</p>';
+		//output.innerHTML = html;
+		//console.log("Cmd hang");
+		
+		$("msg").firstChild.nodeValue = "Possible network error. Will retry after some time.";
+		
+		clearInterval(slider);	
+		hide("data");
+		show("wait");		
+		
+		return
+	} 
+}
+
+function startSlide(count) {
+	var cls;
+	var dt;
+	var dd;
+	var done;
+	var tempDt;
+	var tempDd;
+	
+	done = false;
+	tempDt = [];
+	tempDd = [];
+
+	dt = $("rateSlides").getElementsByTagName('dt');
+	dd = $("rateSlides").getElementsByTagName('dd');
+
+	for (var e=0; e < dt.length; e++) {
+		tempDt[tempDt.length] = dt[e];
+	}
+	
+	for (var i = 0; i < tempDt.length; i++) {
+		if (done) { continue; }
+		
+		cls = tempDt[i].className;
+		//console.log("ClassDt: " + i + cls);
+		
+		if ((cls.indexOf("current")) != -1) {
+			//console.log("true");
+			
+			tempDt[i].className = "";
+			
+			if (i == (tempDt.length-1)) {
+				tempDt[0].className = 'current';
+			} else {
+				tempDt[i+1].className = 'current';
+			}
+			
+			done = true;
+		}
+	}
+
+	tempDt = null;
+	done = false;
+
+	for (var s=0; s < dd.length; s++) {
+		tempDd[tempDd.length] = dd[s];
+	}
+	
+	for (var t = 0; t < tempDd.length; t++) {
+		if (done) { continue; }
+		
+		cls = tempDd[t].className;
+		//console.log("ClassDd: " + t + cls);
+		
+		if ((cls.indexOf("current")) != -1) {
+			//console.log("true");
+			
+			tempDd[t].className = "";
+			//console.log("Replaced: " + tempDd[t].className);
+			if (t === (tempDd.length-1)) {
+				tempDd[0].className = 'current';
+				//console.log("New: " + tempDd[0].className);
+			} else {
+				tempDd[t+1].className = 'current';
+				//console.log("New: " + tempDd[t+1].className);
+			}
+			done = true;
+		}
+	}
+	
+	tempDd = null;
+}
+
+function reconfigure(e) {
+	if (e.storageArea != widget.preferences) return;
+	switch(e.key) {
+		case 'interval': setRefreshTimer(); break;
+	}
+}
+
+function setRefreshTimer() {
+	clearInterval(slider);
+	slider = setInterval(startSlide, 5000);
+}
+
+function init() {
+	console.log("Initialized");
+	console.log("Pair: " + widget.preferences.pairs);
+	
+	if (!widget.preferences.pairs) {
+		var c = ["USD/EUR"];
+		widget.preferences.pairs = JSON.stringify(c);
+	}
+	
+	window.addEventListener('storage', reconfigure, false);
+	timeIt = setInterval(getData, parseInt(widget.preferences.interval) * 60 * 1000);
+	
+	getData();
+}
+
+document.addEventListener('DOMContentLoaded', init, false);
---
+++ source/js/options.js
@@ -0,0 +1,455 @@
+﻿var pairs = widget.preferences.pairs;
+var interval = widget.preferences.interval;
+var max = widget.preferences.maxpairs;
+var stack = {};
+var count;
+
+var currency = {
+	"EUR": "Euro",
+	"GBP": "British Pound",
+	"USD": "United States Dollar",
+	"AUD": "Australian Dollar",
+	"CAD": "Canadian Dollar",
+	"CHF": "Swiss Franc",
+	"CNY": "Chinese Yuan",
+	"HKD": "Hong Kong Dollar",
+	"IDR": "Indonesian Rupiah",
+	"INR": "Indian Rupee",
+	"JPY": "Japanese Yen",
+	"THB": "Thai Baht",
+	"ALL": "Albanian Lek",
+	"DZD": "Algerian Dinar",
+	"XAL": "Aluminium Ounces",
+	"ARS": "Argentine Peso",
+	"AWG": "Aruba Florin",
+	"BSD": "Bahamian Dollar",
+	"BHD": "Bahraini Dinar",
+	"BDT": "Bangladesh Taka",
+	"BBD": "Barbados Dollar",
+	"BYR": "Belarus Ruble",
+	"BZD": "Belize Dollar",
+	"BMD": "Bermuda Dollar",
+	"BTN": "Bhutan Ngultrum",
+	"BOB": "Bolivian Boliviano",
+	"BWP": "Botswana Pula",
+	"BRL": "Brazilian Real",
+	"BND": "Brunei Dollar",
+	"BGN": "Bulgarian Lev",
+	"BIF": "Burundi Franc",
+	"KHR": "Cambodia Riel",
+	"CVE": "Cape Verde Escudo",
+	"KYD": "Cayman Islands Dollar",
+	"XOF": "CFA Franc (BCEAO)",
+	"XAF": "CFA Franc (BEAC)",
+	"CLP": "Chilean Peso",
+	"COP": "Colombian Peso",
+	"KMF": "Comoros Franc",
+	"XCP": "Copper Pounds",
+	"CRC": "Costa Rica Colon",
+	"HRK": "Croatian Kuna",
+	"CUP": "Cuban Peso",
+	"CZK": "Czech Koruna",
+	"DKK": "Danish Krone",
+	"DJF": "Dijibouti Franc",
+	"DOP": "Dominican Peso",
+	"XCD": "East Caribbean Dollar",
+	"ECS": "Ecuador Sucre",
+	"EGP": "Egyptian Pound",
+	"SVC": "El Salvador Colon",
+	"ERN": "Eritrea Nakfa",
+	"EEK": "Estonian Kroon",
+	"ETB": "Ethiopian Birr",
+	"FKP": "Falkland Islands Pound",
+	"FJD": "Fiji Dollar",
+	"GMD": "Gambian Dalasi",
+	"GHC": "Ghanian Cedi",
+	"GIP": "Gibraltar Pound",
+	"XAU": "Gold Ounces",
+	"GTQ": "Guatemala Quetzal",
+	"GNF": "Guinea Franc",
+	"GYD": "Guyana Dollar",
+	"HTG": "Haiti Gourde",
+	"HNL": "Honduras Lempira",
+	"HUF": "Hungarian Forint",
+	"ISK": "Iceland Krona",
+	"IRR": "Iran Rial",
+	"IQD": "Iraqi Dinar",
+	"ILS": "Israeli Shekel",
+	"JMD": "Jamaican Dollar",
+	"JOD": "Jordanian Dinar",
+	"KZT": "Kazakhstan Tenge",
+	"KES": "Kenyan Shilling",
+	"KRW": "South Korean Won",
+	"KWD": "Kuwaiti Dinar",
+	"LAK": "Lao Kip",
+	"LVL": "Latvian Lat",
+	"LBP": "Lebanese Pound",
+	"LSL": "Lesotho Loti",
+	"LRD": "Liberian Dollar",
+	"LYD": "Libyan Dinar",
+	"LTL": "Lithuanian Lita",
+	"MOP": "Macau Pataca",
+	"MKD": "Macedonian Denar",
+	"MWK": "Malawi Kwacha",
+	"MYR": "Malaysian Ringgit",
+	"MVR": "Maldives Rufiyaa",
+	"MTL": "Maltese Lira",
+	"MRO": "Mauritania Ougulya",
+	"MUR": "Mauritius Rupee",
+	"MXN": "Mexican Peso",
+	"MDL": "Moldovan Leu",
+	"MNT": "Mongolian Tugrik",
+	"MAD": "Moroccan Dirham",
+	"MMK": "Myanmar Kyat",
+	"NAD": "Namibian Dollar",
+	"NPR": "Nepalese Rupee",
+	"ANG": "Neth Antilles Guilder",
+	"TRY": "Turkish Lira",
+	"NZD": "New Zealand Dollar",
+	"NIO": "Nicaragua Cordoba",
+	"NGN": "Nigerian Naira",
+	"KPW": "North Korean Won",
+	"NOK": "Norwegian Krone",
+	"OMR": "Omani Rial",
+	"XPF": "Pacific Franc",
+	"PKR": "Pakistani Rupee",
+	"XPD": "Palladium Ounces",
+	"PAB": "Panama Balboa",
+	"PGK": "Papua New Guinea Kina",
+	"PYG": "Paraguayan Guarani",
+	"PEN": "Peruvian Nuevo Sol",
+	"PHP": "Philippine Peso",
+	"XPT": "Platinum Ounces",
+	"PLN": "Polish Zloty",
+	"QAR": "Qatar Rial",
+	"RON": "Romanian New Leu",
+	"RUB": "Russian Rouble",
+	"RWF": "Rwanda Franc",
+	"WST": "Samoa Tala",
+	"STD": "Sao Tome Dobra",
+	"SAR": "Saudi Arabian Riyal",
+	"SCR": "Seychelles Rupee",
+	"SLL": "Sierra Leone Leone",
+	"XAG": "Silver Ounces",
+	"SGD": "Singapore Dollar",
+	"SKK": "Slovak Koruna",
+	"SIT": "Slovenian Tolar",
+	"SBD": "Solomon Islands Dollar",
+	"SOS": "Somali Shilling",
+	"ZAR": "South African Rand",
+	"LKR": "Sri Lanka Rupee",
+	"SHP": "St Helena Pound",
+	"SDG": "Sudanese Pound",
+	"SZL": "Swaziland Lilageni",
+	"SEK": "Swedish Krona",
+	"SYP": "Syrian Pound",
+	"TWD": "Taiwan Dollar",
+	"TZS": "Tanzanian Shilling",
+	"TOP": "Tonga Pa'ang",
+	"TTD": "Trinidad & Tobago Dollar",
+	"TND": "Tunisian Dinar",
+	"AED": "UAE Dirham",
+	"UGX": "Ugandan Shilling",
+	"UAH": "Ukraine Hryvnia",
+	"UYU": "Uruguayan New Peso",
+	"VUV": "Vanuatu Vatu",
+	"VEF": "Venezuelan Bolivar Fuerte",
+	"VND": "Vietnam Dong",
+	"YER": "Yemen Riyal",
+	"ZMK": "Zambian Kwacha",
+	"ZWD": "Zimbabwe dollar"
+};	
+
+function $(v) {
+	if (document.getElementById(v)) {
+		return document.getElementById(v);
+	}
+}
+
+function hide(id) {
+	$(id).style.display = 'none';
+}
+
+function show(id) {
+	$(id).style.display = 'block';
+}
+
+function E(v) {
+	return document.createElement(v);
+}
+
+function Txt(v) {
+	return document.createTextNode(v);
+}	
+
+function status(msg) {
+	var hangTimer;
+	
+	$("msg").firstChild.nodeValue = msg;
+	show("msg");
+	
+	// show status update for 5 seconds
+	clearTimeout(hangTimer);
+	hangTimer = setTimeout(function() {
+		hide("msg");
+	}, 10000);	
+}
+
+function unlock() {
+	if (count == 0) {
+		$('apply').disabled = true;
+	} else {
+		$('apply').disabled = false;
+	}
+}
+
+function apply() {
+	var cmd, i, temp;
+	var ul, li;
+	var save = false;
+	
+	i = document.input.interval.value;
+	i = parseInt(i);		
+
+	if (!i) { 
+		// Validation - interval should be a number
+		status("Error: Update interval should be a number");
+		return;
+	} else {
+		document.input.interval.value = i;
+	}
+	
+	if (i < 15) {
+		// Validation - interval cannot be less than 15
+		status("Error: Update interval should be more than 15 minutes.");
+		return;			
+	}
+	
+	console.log("Stack: " + JSON.stringify(stack));
+	console.log("Pairs: " + pairs);
+	
+	for (var id in stack) {
+	
+		temp = pairs.indexOf(id);
+		
+		if (stack[id] == 'add'){
+			if (temp != -1) {
+				// Validation - check for duplicate
+				status('Error: Duplicate pair detected - ' + id);
+				return;
+			} else {
+				console.log("oTypeA: " + typeof(pairs));			
+				pairs[pairs.length] = id;
+				save = true;
+			}				
+		}
+		
+		if (stack[id] == 'remove') {
+			if (temp != -1) {
+				console.log("oTypeR: " + typeof(pairs));
+				pairs.splice(temp, 1);				
+				save = true;
+			}	
+		}
+	}
+	
+	console.log("pType: " + typeof(pairs));
+	
+	// save changes
+	if (save) {		
+		widget.preferences.pairs = JSON.stringify(pairs);
+	}
+
+	if (i != interval) {
+		widget.preferences.interval = i;
+		console.log("Interval: " + i);		
+	}	
+	
+	stack = {}
+	
+	pairs = widget.preferences.pairs;
+	console.log("Stored: " + pairs);
+	
+	hide("set");
+
+	ul = $("set");
+	li = ul.getElementsByTagName("li");
+	
+	// remove class name to flag that
+	// pairs have been saved
+	for (var e = 0; e < li.length; e++) {
+		li[e].className = "";
+	}
+	
+	show("set");
+	
+	if (save) {
+		// reload dial with new settings
+		opera.extension.bgProcess.getData();		
+	}
+	
+	return;
+}
+
+function removePair() {
+	var id, li;
+	var temp;
+	
+	id = this.parentNode.id;
+	
+	li = $(id);
+
+	// makes sure we don't delete a pair
+	// that is not saved in preferences			
+	
+	if (li.className == 'new') {
+		// delete from stack only
+		delete stack[id];
+		console.log("deleted from stack");
+	} else {
+		// mark for deletion
+		stack[id] = 'remove';
+		console.log("marked for deletion");
+	}
+	
+	count = count - 1;
+	
+	if (count == 0) {
+		$('apply').disabled = true;
+	} else {
+		$('apply').disabled = false;
+	}
+	
+	li.parentNode.removeChild(li);
+}
+
+function addPair() {
+	var temp, first, second;
+	var li, id, a, txt;
+	
+	if (count == max) {
+		// validation - 5+ pair not allowed
+		status("Error: Maximum limit of " + max + " pairs reached.");
+		return;	
+	}
+	
+	first = document.input.first.value;
+	second = document.input.second.value;
+	
+	if (first === second) {
+		// Validation - same currency selection
+		status("Error: Same currency selected.");
+		return;
+	}
+		
+	id = first + "/" + second;
+	
+	if (stack[id]) {
+		// Validation - Display shouldn't show duplicates
+		if (!(stack['id'] == 'remove')) {
+			status('Error: Duplicate pair not added - ' + id);
+			return;
+		}
+	}
+	
+	if (pairs.indexOf(id) != -1) {
+		// Validation - Display shouldn't show duplicates
+		status('Error: Duplicate pair not added - ' + id);
+		return;
+	}
+	
+	li = E("li");
+	li.setAttribute('id', id);
+	li.className = "new";
+	
+	a = E("a");
+	a.setAttribute('href', '#self');
+	txt = Txt('delete');
+	a.appendChild(txt);
+	a.addEventListener('click', removePair, false);
+	
+	txt = first;
+	txt += " / " + second;
+	txt += " (" + currency[first];
+	txt	+= " / " + currency[second] + ")";
+	txt = Txt(txt);
+	
+	li.appendChild(a);
+	li.appendChild(txt);
+	
+	// if a pair saved in preferences
+	// was to be deleted, and that same pair
+	// has been added again undo the remove command		
+	
+	if (stack[id]) {
+		delete stack[id];
+	} else {
+		stack[id] = "add"
+	}
+	
+	count += 1;
+		
+	$("set").appendChild(li);
+	$('apply').disabled = false;
+	
+	return;
+}
+
+function load() {		
+	var temp, first, second;
+	
+	var ul, li, a, txt;
+	var inHtml = document.createDocumentFragment();
+
+	hide("set");
+	
+	for (var i = 0; i < count; i++) {
+		li = E("li");
+		
+		a = E("a");
+		a.setAttribute('href', '#self' + i);
+		txt = Txt('delete');
+		a.appendChild(txt);
+		a.addEventListener('click', removePair, false);
+		
+		temp = pairs[i];
+		temp = temp.split('/');
+		
+		first = temp[0];
+		second = temp[1];
+		
+		li.setAttribute('id', first + "/" + second);
+		
+		txt = first;
+		txt += " / " + second;
+		txt += " (" + currency[first];
+		txt	+= " / " + currency[second] + ")";
+			
+		txt = Txt(txt);
+		
+		li.appendChild(a);
+		li.appendChild(txt);
+		
+		inHtml.appendChild(li);
+	}
+	
+	$("set").appendChild(inHtml);
+	show("set");
+		
+	document.input.interval.value = interval;
+}
+
+function init() {
+	pairs = JSON.parse(pairs);
+	count = pairs.length;
+	interval = parseInt(interval);
+	
+	$('apply').disabled = true;
+	$('addpair').addEventListener('click', addPair, false); 
+	$('apply').addEventListener('click', apply, false);
+	$('interval').addEventListener('keypress', unlock ,false);
+	
+	load();
+}
+
+document.addEventListener('DOMContentLoaded', init, false);
